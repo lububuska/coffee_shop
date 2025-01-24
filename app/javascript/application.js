2d4097b57b9d0723c8cd7cf67e39de7347dde0df
Мария Масенко <mariamasenko@Marias-MacBook-Air.local>
2025-01-25 00:36:24 +0300

    add orders views and some refactoring

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,2 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "orders"
-import "@hotwired/turbo-rails"
-import "controllers"
-import "orders"
+import "@rails/ujs";
+Rails.start();
